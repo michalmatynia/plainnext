a74fbf286bf8f3fb6b6703b1128f4380222d8816
Michal <mmatynia@gmail.com>
2023-02-16 21:27:25 +0100

    first component

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const next = require('next');
+const express = require('express')
+const next = require('next')
 
-const dev = process.env.NODE_ENV !== 'production';
-const nextApp = next({ dev });
-const handle = nextApp.getRequestHandler();
+const dev = process.env.NODE_ENV !== 'production'
+const nextApp = next({ dev })
+const handle = nextApp.getRequestHandler()
 
-const port = 3454;
+const port = 3454
 
 nextApp.prepare().then(() => {
-  const app = express();
+  const app = express()
 
   app.get('*', (req, res) => {
-    return handle(req, res);
-  });
+    return handle(req, res)
+  })
 
-  app.listen(port, err => {
-    if (err) throw err;
-    console.log(`> Ready on localhost:${port}`);
-  });
-});
+  app.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on localhost:${port}`)
+  })
+})
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  framework: {
+    name: process.env.NEXT_PUBLIC_FRAMEWORK,
+  },
+  i18n: {
+    locales: ['en-US', 'es'],
+    defaultLocale: 'en-US',
+  },
 }
 
 module.exports = nextConfig
