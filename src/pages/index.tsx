684a2f3486f61634fb6087cd2559a7317c060001
Michal <mmatynia@gmail.com>
2023-02-16 21:51:30 +0100

    vdfsv

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,6 +18,7 @@ export default function Home(): InferGetStaticPropsType<typeof getStaticProps> {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      ccdsc
       <AppBar />
     </>
   )
