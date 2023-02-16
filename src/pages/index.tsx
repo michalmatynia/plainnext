f8a036ad614ddaa978b8a31537a7e6440d80869e
Michal <mmatynia@gmail.com>
2023-02-16 21:56:11 +0100

    dfsc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home(): InferGetStaticPropsType<typeof getStaticProps> {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      ccdsc
+      Try2
       <AppBar />
     </>
   )
