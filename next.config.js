93f822ea9501806e0bc3fa4ed0dafd4f50de79f5
Michal <mmatynia@gmail.com>
2023-02-21 20:31:56 +0100

    fre

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,6 +9,9 @@ const nextConfig = {
   i18n: {
     locales: ['en-US', 'es'],
     defaultLocale: 'en-US',
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
 }
 
